fb244bb9fa6932f09eb96e4552afe85f9437e38a
Alexander Jaferey <alex@xskj.org>
2023-05-13 03:01:23 +0000

    add next.js frontend with auth0 integration

---
+++ frontend/components/layout.tsx
@@ -0,0 +1,28 @@
+import Head from 'next/head'
+import Header from './header'
+
+type LayoutProps = {
+  user?: any
+  loading?: boolean
+  children: React.ReactNode
+}
+
+const Layout = ({ user, loading = false, children }: LayoutProps) => {
+  return (
+    <>
+      <Head>
+        <title>Next.js with Auth0</title>
+      </Head>
+
+      <Header user={user} loading={loading} />
+
+      <main className="m-6 text-zinc-800 py-4 px-6 justify-between grid grid-cols-4 font-global">
+        <div className="col-span-1"></div>
+        <div className="max-w-4xl m-6 col-span-2">{children}</div>
+        <div className="col-span-1"></div>
+      </main>
+    </>
+  )
+}
+
+export default Layout
---
+++ frontend/pages/api/auth/[...auth0].tsx
@@ -0,0 +1,15 @@
+import { handleAuth, handleLogin } from '@auth0/nextjs-auth0'
+
+export default handleAuth({
+    login: async (req, res) => {
+        try {
+            await handleLogin(req, res, {
+                authorizationParams: {
+                    audience: 'musicschool',
+                }
+            });
+        }   catch (error) {
+                console.error(error);
+            }
+        }
+});
